c7966b0025f0e9c1685b6a183bcd78987de77d6e
Vydy74 <124319370+Vydy74@users.noreply.github.com>
2024-07-21 05:54:47 +0300

    Add files via upload

---
+++ module_2_3.py
@@ -0,0 +1,14 @@
+my_list = [42, 69, 0, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+new_list = []
+
+i = 0
+
+while i < len(my_list):
+
+    if my_list[i] > 0:
+        new_list.append(my_list[i])
+        i += 1
+    else:
+        break
+
+print(new_list)
