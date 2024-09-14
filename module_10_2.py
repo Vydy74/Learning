9520d3e00340e159214c8158fb2075a8430a1c11
Vydy74 <124319370+Vydy74@users.noreply.github.com>
2024-09-14 16:31:19 +1000

    Add files via upload

---
+++ module_10_2.py
@@ -0,0 +1,37 @@
+from threading import Thread
+from time import sleep
+
+class Knight(Thread):
+    def __init__(self, name, power):
+        super().__init__()
+        self.name = name
+        self.power = power
+        self.enemies = 100
+        self.days = 0
+
+    def run(self):
+        print(f"{self.name}, на нас напали!")
+
+        while self.enemies > 0:
+            self.enemies -= self.power
+            self.days += 1
+            sleep(1)
+            if self.enemies < 0:
+                self.enemies = 0
+            print(f"{self.name} сражается {self.days} день(дня)..., осталось {self.enemies} воинов.")
+
+        print(f"{self.name} одержал победу спустя {self.days} дней(дня)!")
+
+# Создание класса
+first_knight = Knight('Sir Lancelot', 10)
+second_knight = Knight("Sir Galahad", 20)
+
+# Запуск потоков и остановка текущего
+first_knight.start()
+second_knight.start()
+
+first_knight.join()
+second_knight.join()
+
+# Вывод строки об окончании сражения
+print("Все битвы закончились!")
