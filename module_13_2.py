beb8c8b9c7106226f53e4e450378f17c6374daac
Vydy74 <124319370+Vydy74@users.noreply.github.com>
2024-11-04 14:38:59 +1000

    Update module_13_2.py

--- module_13_2.py
+++ module_13_2.py
@@ -14,10 +14,12 @@ dp = Dispatcher()
 
 @dp.message(Command("start"))
 async def cmd_start(message: types.Message):
+    print("Привет! Я бот помогающий твоему здоровью.")
     await message.answer("Привет! Я бот помогающий твоему здоровью.")
 
 @dp.message(F.text)
 async def cmd_message(message: types.Message):
+    print("Введите команду /start, чтобы начать общение.")
     await message.reply("Введите команду /start, чтобы начать общение.")
 
 
