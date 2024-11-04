b566181fcb0d57ea21fdd4a264d1ad339ad93528
Vydy74 <124319370+Vydy74@users.noreply.github.com>
2024-11-04 14:34:14 +1000

    Add files via upload

---
+++ module_13_2.py
@@ -0,0 +1,28 @@
+from aiogram import Bot, Dispatcher, types, F
+from aiogram.enums import ParseMode
+from aiogram.filters.command import Command
+from aiogram.client.default import DefaultBotProperties
+
+import asyncio
+
+
+
+api = ""
+bot = Bot(token=api, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+
+dp = Dispatcher()
+
+@dp.message(Command("start"))
+async def cmd_start(message: types.Message):
+    await message.answer("Привет! Я бот помогающий твоему здоровью.")
+
+@dp.message(F.text)
+async def cmd_message(message: types.Message):
+    await message.reply("Введите команду /start, чтобы начать общение.")
+
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == '__main__':
+    asyncio.run(main())
