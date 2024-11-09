dfa3e447b31e448857f2bbceb8ce2837cbd6a578
Vydy74 <124319370+Vydy74@users.noreply.github.com>
2024-11-09 19:12:25 +1000

    Add files via upload

---
+++ module_13_4.py
@@ -0,0 +1,52 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters.state import State, StatesGroup
+
+
+api = ""
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+class UserState(StatesGroup):
+    age = State()
+    growth = State()
+    weight = State()
+
+# Обработчик команды /start
+@dp.message_handler(commands=['start'])
+async def start_command(message: types.Message):
+    await message.reply("Привет! Для расчета нормы калорий напишите 'Calories'.")
+
+
+@dp.message_handler(text = "Calories")
+async def set_age(message):
+    await message.answer("Отправь свой возраст в годах")
+    await UserState.age.set()
+
+@dp.message_handler(state=UserState.age)
+async def set_growth(message, state):
+    await state.update_data(age = message.text)
+    await message.answer("Введите свой рост в см")
+    await UserState.growth.set()
+
+@dp.message_handler(state = UserState.growth)
+async def set_weight(message, state):
+    await state.update_data(growth = message.text)
+    await message.answer("Введите свой вес в кг")
+    await UserState.weight.set()
+
+@dp.message_handler(state=UserState.weight)
+async def send_calories(message, state):
+    await state.update_data(weight = message.text)
+    data = await state.get_data()
+    age = int(data.get('age'))
+    growth = float(data.get('growth'))
+    weight = float(data.get('weight'))
+
+    # формула Миффлина - Сан Жеора для женщин
+    calories = (10 * weight) + (6.25 * growth) - (5 * age) - 161
+    await message.answer(f"Ваша суточная норма калорий = {calories} калорий")
+    await state.finish()
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
