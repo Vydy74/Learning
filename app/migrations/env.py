1cbe474c15369e727c69ce346de5ef01de525002
Vydy74 <124319370+Vydy74@users.noreply.github.com>
2025-01-05 14:59:26 +0400

    Add files via upload

--- app/migrations/env.py
+++ app/migrations/env.py
@@ -4,6 +4,9 @@ from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
+from app.backend.db import Base
+from app.models.user import User
+from app.models.task import Task
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -18,12 +21,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-from app.backend.db import Base #---------
-from app.models.task import Task
-from app.models.user import User
-target_metadata = Base.metadata #----------
-# alembic revision --autogenerate -m "initial migration"
-# Команда для запуска миграции
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
